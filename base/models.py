dad13864ff1105231ca5d6e3cd71cac0606a3cbd
IBRAHIM LANRE <ibrahimlanre1890@gmail.com>
2023-05-09 09:21:28 +0100

    second commit

--- base/models.py
+++ base/models.py
@@ -2,29 +2,34 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    name = models.CharField(max_length = 100 , null= True)
-    email = models.EmailField(unique= True, null = True)
-    bio = models.TextField(null =True , blank = True)
+    name = models.CharField(max_length = 100 , null= True) # the datasets are given for the name  
+    email = models.EmailField(unique= True, null = True)#  the datasers that are given in the email field 
+    bio = models.TextField(null =True , blank = True) # the databse that handles the the bio field 
+    # creawting databases that are given in the code base 
+
+
+    hackathon_participants = models.BooleanField(default = True , null = True)
     # avatar =
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email' # usigng the usernames as emails for the login pages
+    REQUIRED_FIELDS = ['username'] # using the required fields as names
 
+# data bases for users to have events in the hackathon
 class Events(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField(null= True, blank = True )
-    participants = models.ManyToManyField(User , blank= True)
-    created = models.DateTimeField(auto_now_add=True)
-    date = models.DateTimeField()
-    updated = models.DateTimeField(auto_now =True)
+    name = models.CharField(max_length=200) # datasets for names in the events 
+    description = models.TextField(null= True, blank = True ) # datasets for the description in the events 
+    participants = models.ManyToManyField(User , blank= True) # dataset for viewing the description inthe events 
+    created = models.DateTimeField(auto_now_add=True) #for adding time stamps
+    date = models.DateTimeField() # for viewing the time the user created the events
+    updated = models.DateTimeField(auto_now =True) # for viewing the updated 
 
     def __str__(self):
-        return self.name
+        return self.name # returning the string name of the events 
     
 class submission(models.Model):
-    participants = models.ForeignKey(User, on_delete=models.SET_NULL ,null=True)
-    event = models.ForeignKey(Events,  on_delete= models.CASCADE)
-    details  = models.TextField(null= True , blank = True)
+    participants = models.ForeignKey(User, on_delete=models.SET_NULL ,null=True) #datasets for patitcipants to submit the event registered 
+    event = models.ForeignKey(Events,  on_delete= models.CASCADE)# to know the events you are submitting 
+    details  = models.TextField(null= True , blank = True)# the details about the user for the submission
 
     def __str__(self):
         return str(self.event ) + '---'+ str(self.participants)
--- base/views.py
+++ base/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import User , Events , submission
 
 # Create your views here.
 def homepage(request):
-    return render(request  , 'home.html')
+    users = User.objects.filter(hackathon_participants = True) # to filter users using the events only 
+    events = Events.objects.all() # to gets all events registered in the database 
+    context = {'users': users , 'events': events}
+    return render(request  , 'home.html',context)
+
+def events_page(request , pk):
+    event = Events.objects.get(id = pk)
+    context = {'event':event}
+    return render(request, 'events.html', context)
+
+
+def registration_confirm(request):
+    pass
