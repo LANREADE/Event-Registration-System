d47059ecaadfae4eb165904455b45bb848bc7d0c
IBRAHIM LANRE <ibrahimlanre1890@gmail.com>
2023-05-13 15:29:59 +0100

    3rd commit

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('' , views.homepage , name="home" ),
-    path('event/<str:pk>' , views.events_page , name = "event")
+    path('user/<str:pk>/', views.user_page , name= "profile"),
+    path('event/<str:pk>/' , views.events_page , name = "event"),
+    path('registration-confirmation/<str:pk>/', views.registration_confirm, name = "registration-confirmation")
 ]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User , Events , submission
 
 # Create your views here.
@@ -8,11 +8,23 @@ def homepage(request):
     context = {'users': users , 'events': events}
     return render(request  , 'home.html',context)
 
+
+def user_page(request , pk):
+    user = User.objects.get(id = pk)
+    context = {'user': user}
+    return render(request , 'profile.html', context)
+
+
 def events_page(request , pk):
     event = Events.objects.get(id = pk)
     context = {'event':event}
     return render(request, 'events.html', context)
 
 
-def registration_confirm(request):
-    pass
+def registration_confirm(request,pk):
+    event = Events.objects.get(id =pk)
+    if request.method == 'POST':
+        event.participants.add(request.user)
+        return redirect('event', pk=event.id)
+    
+    return render(request , 'event_confirmation.html' , {'event': event})
